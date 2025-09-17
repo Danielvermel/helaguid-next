e5a3e7fb28537f32b0d89f621c5e926e2ecfdace
suue99 <susan.olofin@gmail.com>
2025-09-17 13:26:11 +0100

    Practitioner details updated + broken links identified

--- src/constants/carousel.jsx
+++ src/constants/carousel.jsx
@@ -1,90 +1,90 @@
 export const practitioners = [
-    {
-        id: 1,
-        name: "Ignacio G.",
-        specialty: "Chronic Pain & Recovery",
-        imageUrl: "/images/practioners/ignacio-g.jpg", // Path in your /public folder
-        profileUrl:
-            "https://book.healguid.com/practitioners/ignacio-garcia-osteopathic-practitioner-united-states/r/wX6rvGCDbAZpFy", // The Next.js page for this practitioner
-    },
-    {
-        id: 2,
-        name: "Jonas M.",
-        specialty: "Chronic Fatigue & Energy Issues",
-        imageUrl: "/images/practioners/jonas-m.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/jonas-m-ller-integrative-medicine-practitioner-germany/r/kBCD2YJvmnjkhd",
-    },
-    {
-        id: 3,
-        name: "Tatiana P.",
-        specialty: "Hormonal Imbalances",
-        imageUrl: "/images/practioners/tatiana-p.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/tatiana-patel-functional-medicine-practitioner-united-kingdom/r/oU8S4S7PRJpfeK",
-    },
-    {
-        id: 4,
-        name: "Lucia D.",
-        specialty: "Hormonal Imbalances",
-        imageUrl: "/images/practioners/lucia-d.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/lucia-de-santos-naturopathic-practitioner-united-kingdom/r/mj0ehowZ7NfQuk",
-    },
-    {
-        id: 5,
-        name: "Maria R.",
-        specialty: "Chronic Fatigue & Energy Issues",
-        imageUrl: "/images/practioners/maria-r.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/maria-reyes-functional-medicine-practitioner-united-states/r/HKnsE2QjgEjOXC",
-    },
-    {
-        id: 6,
-        name: "Maya O.",
-        specialty: "Anxiety & Mental Wellbeing",
-        imageUrl: "/images/practioners/maya-o.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/maya-olivier-functional-medicine-practitioner-france/r/jrfsGgkz8OvhxV",
-    },
-    {
-        id: 7,
-        name: "Naomi F.",
-        specialty: "Preventive Health & Optimization",
-        imageUrl: "/images/practioners/naomi-f.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/naomi-feldman-nutritional-therapist-ireland/r/s6h5mOjqRBmQ5n",
-    },
-    {
-        id: 8,
-        name: "Ravi S.",
-        specialty: "Preventive Health & Optimization",
-        imageUrl: "/images/practioners/ravi-s.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/ravi-singh-nutritional-therapist-united-kingdom/r/X6SCDn50ZuzKXs",
-    },
-    {
-        id: 9,
-        name: "Samuel O.",
-        specialty: "Preventive Health & Optimization",
-        imageUrl: "/images/practioners/samuel-o.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/samuel-ofori-naturopathic-practitioner-united-kingdom/r/WQcxqOPg5vuPFd",
-    },
-    {
-        id: 10,
-        name: "Sarah C.",
-        specialty: "Digestive & Gut Health",
-        imageUrl: "/images/practioners/sarah-c.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/sarah-chen-functional-medicine-practitioner-united-kingdom/r/cRMnhqnXbeRcQf",
-    },
-    {
-        id: 11,
-        name: "Julia K.",
-        specialty: "Digestive & Gut Health",
-        imageUrl: "/images/practioners/julia-k.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/julia-kim-functional-medicine-practitioner-united-states/r/drrZhyPDTbcWxJ",
-    },
+  {
+    id: 1,
+    name: "Benjamin R.",
+    specialty: "Nutritional Therapist",
+    imageUrl: "/images/practioners/benjamin-r.jpg", // Path in your /public folder
+    profileUrl:
+      "https://book.healguid.com/practitioners/benjamin-richardson-nutritional-therapist-united-kingdom/r/CaA50bpIg2DCLa", // The Next.js page for this practitioner
+  },
+  {
+    id: 2,
+    name: "Ines J.",
+    specialty: "Nutritional Therapist",
+    imageUrl: "/images/practioners/ines-j.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/ines-jabir-nutritional-therapist-united-kingdom/r/Btf2gh2T8J2cqk",
+  },
+  {
+    id: 3,
+    name: "Yusuf R.",
+    specialty: "Functional Medicine Practitioner",
+    imageUrl: "/images/practioners/yusuf-r.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/yusuf-rushdi-functional-medicine-practitioner-united-kingdom/r/FJKCEuu8iQYUMB",
+  },
+  {
+    id: 4,
+    name: "Steven F.",
+    specialty: "Mind-Body Wellness Practitioner",
+    imageUrl: "/images/practioners/steven-f.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/steven-ford-mind-body-wellness-practitioner-united-kingdom/r/p9LPx6kTOHj95W",
+  },
+  {
+    id: 5,
+    name: "Lucia S.",
+    specialty: "Nutritional Therapist",
+    imageUrl: "/images/practioners/lucia-s.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/lucia-stansbie-nutritional-therapist-united-kingdom/r/UxE7KAcpjlAdxT",
+  },
+  {
+    id: 6,
+    name: "Taiba G.",
+    specialty: "Functional Medicine Practitioner",
+    imageUrl: "/images/practioners/taiba-g.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/taiba-gul-functional-medicine-practitioner-united-kingdom/r/9tEiqAcaUvhaPr",
+  },
+  {
+    id: 7,
+    name: "Christelle S.",
+    specialty: "Functional Medicine Practitioner",
+    imageUrl: "/images/practioners/christelle-s.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/christelle-stockdale-functional-medicine-practitioner-united-kingdom/r/en5dEK0cxgsVgw",
+  },
+  {
+    id: 8,
+    name: "Ravi S.",
+    specialty: "Preventive Health & Optimization",
+    imageUrl: "/images/practioners/ravi-s.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/ravi-singh-nutritional-therapist-united-kingdom/r/X6SCDn50ZuzKXs",
+  },
+  {
+    id: 9,
+    name: "Samuel O.",
+    specialty: "Preventive Health & Optimization",
+    imageUrl: "/images/practioners/samuel-o.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/samuel-ofori-naturopathic-practitioner-united-kingdom/r/WQcxqOPg5vuPFd",
+  },
+  {
+    id: 10,
+    name: "Sarah C.",
+    specialty: "Digestive & Gut Health",
+    imageUrl: "/images/practioners/sarah-c.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/sarah-chen-functional-medicine-practitioner-united-kingdom/r/cRMnhqnXbeRcQf",
+  },
+  {
+    id: 11,
+    name: "Julia K.",
+    specialty: "Digestive & Gut Health",
+    imageUrl: "/images/practioners/julia-k.jpg",
+    profileUrl:
+      "https://book.healguid.com/practitioners/julia-kim-functional-medicine-practitioner-united-states/r/drrZhyPDTbcWxJ",
+  },
 ];
