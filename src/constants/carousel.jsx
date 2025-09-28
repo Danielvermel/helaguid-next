cd3266f87392599b1e87eae4169373088924f825
Daniel Leite <daniel.f.m.leite@gmail.com>
2025-09-28 22:45:56 +0100

    update card links

--- src/constants/carousel.jsx
+++ src/constants/carousel.jsx
@@ -4,42 +4,42 @@ export const practitioners = [
         name: "Benjamin R.",
         specialty: "Nutritional Therapist",
         imageUrl: "/images/practioners/benjamin-r.jpg", // Path in your /public folder
-        profileUrl: "/practitioners/benjamin-richardson-nutritional-therapist-united-kingdom/r/CaA50bpIg2DCLa", // The Next.js page for this practitioner
+        profileUrl: "/practitioners?recordId=CaA50bpIg2DCLa", // The Next.js page for this practitioner
     },
     {
         id: 2,
         name: "Ines J.",
         specialty: "Nutritional Therapist",
         imageUrl: "/images/practioners/ines-j.jpg",
-        profileUrl: "/practitioners/ines-jabir-nutritional-therapist-united-kingdom/r/Btf2gh2T8J2cqk",
+        profileUrl: "/practitioners/r/Btf2gh2T8J2cqk",
     },
     {
         id: 3,
         name: "Yusuf R.",
         specialty: "Functional Medicine Practitioner",
         imageUrl: "/images/practioners/yusuf-r.jpg",
-        profileUrl: "/practitioners/yusuf-rushdi-functional-medicine-practitioner-united-kingdom/r/FJKCEuu8iQYUMB",
+        profileUrl: "/practitioners/r/FJKCEuu8iQYUMB",
     },
     {
         id: 4,
         name: "Steven F.",
         specialty: "Mind-Body Wellness Practitioner",
         imageUrl: "/images/practioners/steven-f.jpg",
-        profileUrl: "/practitioners/steven-ford-mind-body-wellness-practitioner-united-kingdom/r/p9LPx6kTOHj95W",
+        profileUrl: "/practitioners/r/p9LPx6kTOHj95W",
     },
     {
         id: 5,
         name: "Lucia S.",
         specialty: "Nutritional Therapist",
         imageUrl: "/images/practioners/lucia-s.jpg",
-        profileUrl: "/practitioners/lucia-stansbie-nutritional-therapist-united-kingdom/r/UxE7KAcpjlAdxT",
+        profileUrl: "/practitioners/r/UxE7KAcpjlAdxT",
     },
     {
         id: 6,
         name: "Taiba G.",
         specialty: "Functional Medicine Practitioner",
         imageUrl: "/images/practioners/taiba-g.jpg",
-        profileUrl: "/practitioners/taiba-gul-functional-medicine-practitioner-united-kingdom/r/9tEiqAcaUvhaPr",
+        profileUrl: "/practitioners/r/9tEiqAcaUvhaPr",
     },
     {
         id: 7,
@@ -47,34 +47,34 @@ export const practitioners = [
         specialty: "Functional Medicine Practitioner",
         imageUrl: "/images/practioners/christelle-s.jpg",
         profileUrl:
-            "/practitioners/christelle-stockdale-functional-medicine-practitioner-united-kingdom/r/en5dEK0cxgsVgw",
+            "/practitioners?recordId=YHTcQSEdUIxYZZ",
     },
     {
         id: 8,
         name: "Ravi S.",
         specialty: "Preventive Health & Optimization",
         imageUrl: "/images/practioners/ravi-s.jpg",
-        profileUrl: "/practitioners/ravi-singh-nutritional-therapist-united-kingdom/r/X6SCDn50ZuzKXs",
+        profileUrl: "/practitioners/r/X6SCDn50ZuzKXs",
     },
     {
         id: 9,
         name: "Samuel O.",
         specialty: "Preventive Health & Optimization",
         imageUrl: "/images/practioners/samuel-o.jpg",
-        profileUrl: "/practitioners/samuel-ofori-naturopathic-practitioner-united-kingdom/r/WQcxqOPg5vuPFd",
+        profileUrl: "/practitioners/r/WQcxqOPg5vuPFd",
     },
     {
         id: 10,
         name: "Sarah C.",
         specialty: "Digestive & Gut Health",
         imageUrl: "/images/practioners/sarah-c.jpg",
-        profileUrl: "/practitioners/sarah-chen-functional-medicine-practitioner-united-kingdom/r/cRMnhqnXbeRcQf",
+        profileUrl: "/practitioners/r/cRMnhqnXbeRcQf",
     },
     {
         id: 11,
         name: "Julia K.",
         specialty: "Digestive & Gut Health",
         imageUrl: "/images/practioners/julia-k.jpg",
-        profileUrl: "/practitioners/julia-kim-functional-medicine-practitioner-united-states/r/drrZhyPDTbcWxJ",
+        profileUrl: "/practitioners/r/drrZhyPDTbcWxJ",
     },
 ];
