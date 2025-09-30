e08ad73c671d098b61ae2b8292ee9d9b74bbb52d
suue99 <susan.olofin@gmail.com>
2025-09-30 17:49:09 +0100

    link fix

--- src/constants/carousel.jsx
+++ src/constants/carousel.jsx
@@ -1,72 +1,66 @@
 export const practitioners = [
     {
         id: 1,
-        name: "Taiba G.",
-        specialty: "Functional Medicine Practitioner",
-        imageUrl: "/images/practioners/taiba-g.jpg",
-        profileUrl:
-            "/https://book.healguid.com/practitioners/taiba-gul-functional-medicine-practitioner-united-kingdom/r/9tEiqAcaUvhaPr",
-        bio: "Having trained in Conventional Medicine, I very quickly realised that the approach to people needs to be from a holistic point of view. I moved in to General Practice and started looking at all the factors that were impacting my patients including their lifestyle, their diet and their wider environment. My focus on health promotion and my background in health coaching worked very well for my patients and I found my passion. This lead me to training under the Institute of Functional Medicine where I learnt to look for the ‘root cause’ behind the ‘pathology’.",
-        ratings: "5",
-    },
-    {
-        id: 2,
-        name: "Christelle S.",
-        specialty: "Functional Medicine Practitioner",
-        imageUrl: "/images/practioners/christelle-s.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/christelle-stockdale-functional-medicine-practitioner-united-kingdom/r/YHTcQSEdUIxYZZ",
-        bio: "I was drawn to holistic and functional medicine because I wanted to help people get to the true root of their health issues. Not just manage symptoms but understand why they’re unwell and how to restore balance naturally. What makes my approach unique? I combine deep listening with detailed investigation. My focus is on treating the whole person, creating clear, realistic plans, and empowering my patients to take charge of their own health. What do patients value most? They appreciate that I truly listen, explain things clearly, and work with them to make treatment plans that are manageable. Many say they finally feel seen, understood, and supported. ",
-        ratings: "5",
-    },
-    {
-        id: 3,
         name: "Benjamin R.",
         specialty: "Nutritional Therapist",
         imageUrl: "/images/practioners/benjamin-r.jpg", // Path in your /public folder
-        profileUrl:
-            "https://book.healguid.com/practitioners/benjamin-richardson-nutritional-therapist-united-kingdom/r/CaA50bpIg2DCLa", // The Next.js page for this practitioner
+        profileUrl: "/practitioners/benjamin-richardson-nutritional-therapist-united-kingdom/r/CaA50bpIg2DCLa", // The Next.js page for this practitioner
         bio: "I'm Benjamin, a qualified, regulated, performance focused nutrition & lifestyle medicine practitioner with a particular focus on enhancing healthspan, body composition, sleep and stress. Clients choose me for my dedication to enabling them to progress towards their goals and my commitment to delivery. Shaped by your priorities, enablers and constraints, we co-create for you a personalised, science-based, actionable programme. I act as your strategist, picking the highest value interventions, your coach to help you implement them, and as much as you welcome it, your educator on the *why*.",
         ratings: "5",
     },
     {
-        id: 4,
+        id: 2,
         name: "Ines J.",
         specialty: "Nutritional Therapist",
         imageUrl: "/images/practioners/ines-j.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/ines-jabir-nutritional-therapist-united-kingdom/r/Btf2gh2T8J2cqk",
+        profileUrl: "/practitioners/ines-jabir-nutritional-therapist-united-kingdom/r/Btf2gh2T8J2cqk",
         bio: "Hello everyone! My name is Ines and I am a specialist registered dietitian with a passion to address the full complexity of health through nutrition. My approach is deeply personalised and grounded in both science and compassion. I take time to understand each client’s full story, their physical symptoms, emotional stressors, lifestyle, environment, and goals. Functional and holistic medicine form part of my day-to-day dietetic work as I saw many people, particularly women, doing all the “right” things but still struggling with fatigue, digestive issues, hormonal imbalances, and chronic stress. I knew there had to be a deeper way to heal that doesn’t just treat symptoms but uncovers the root cause and supports the body, mind, and soul. My experience includes both public and private industry and I also like to give back to the community through group sessions in community centres. You can also find some of my favourite recipes on my Instagram page @mynutritionbalance. Looking forward to working together! Ines ;)",
         ratings: "5",
     },
     {
-        id: 5,
+        id: 3,
         name: "Yusuf R.",
         specialty: "Functional Medicine Practitioner",
         imageUrl: "/images/practioners/yusuf-r.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/yusuf-rushdi-functional-medicine-practitioner-united-kingdom/r/FJKCEuu8iQYUMB",
+        profileUrl: "/practitioners/yusuf-rushdi-functional-medicine-practitioner-united-kingdom/r/FJKCEuu8iQYUMB",
         bio: "As a dedicated and compassionate Medical professional with over 22 years of experience, I am committed to providing exceptional patient care. With a solid foundation in diagnosing and treating a wide range of medical conditions, I consistently strive to deliver accurate diagnoses and effective treatment plans. My strong communication skills and ability to build rapport with patients enable me to provide personalised care that promotes overall well-being. Remarkable proficiency in leading a holistic approach to primary healthcare. Competent in spearheading operational excellence, revolutionizing processes, and maximizing resource efficiency to deliver unparalleled patient care. Proficient in driving transformative leadership, crafting robust policies, and ensuring stringent compliance with healthcare standards. Expertise in managing diverse medical conditions, including infectious diseases, chronic illnesses, and trauma, within the constraints of secure facilities, upholding dignity and well-being.",
         ratings: "5",
     },
     {
-        id: 6,
+        id: 4,
+        name: "Steven F.",
+        specialty: "Mind-Body Wellness Practitioner",
+        imageUrl: "/images/practioners/steven-f.jpg",
+        profileUrl: "/practitioners/steven-ford-mind-body-wellness-practitioner-united-kingdom/r/p9LPx6kTOHj95W",
+        bio: "Fourteen years experience in a wide range of therapeutic settings has taught me that change lasts when we consider the whole person—mind, body and relationships. I've worked closely with individuals, groups, families and carers, and have seen how progress accelerates when we explore lived experience and, if applicable, any unresolved trauma, nervous system regulation and related challenging emotions such as shame, guilt, anger, grief, anxiety, depression and/or self esteem. My approach is trauma-informed and practical. I draw on ACT, DBT-informed skills, mindfulness and somatic awareness, together with Internal Family Systems and strengths-based coaching. We’ll clarify your goals and values, explore the systems you’re part of, and use tools you can apply between sessions. Clients value the psychological safety, steady pace and focus on real-life change. My aim is to empower you to lead your own recovery while I guide, challenge and support along the way.",
+        ratings: "5",
+    },
+    {
+        id: 5,
         name: "Lucia S.",
         specialty: "Nutritional Therapist",
         imageUrl: "/images/practioners/lucia-s.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/lucia-stansbie-nutritional-therapist-united-kingdom/r/27XIGqfL9hyeF2",
+        profileUrl: "/practitioners/lucia-stansbie-nutritional-therapist-united-kingdom/r/UxE7KAcpjlAdxT",
         bio: "I am a London-based registered nutritional therapist helping my patients reaching their health goals through the power of personalised nutrition. The foundations of my expertise are rooted in functional medicine, with a detailed understanding of the intricate connections between the different body systems. I employ functional testing, tailored dietary adjustments and targeted nutrition to address the root causes behind a client’s health concerns, collaborating closely with each client to chart a path towards optimum health and well-being. I am a food lover, so my plans are focused on introducing foods and nutrients rather than excluding them and include plenty tasty and easy to execute recipes. Through professional guidance and support I can help you thrive and fall in love with food again!",
         ratings: "5",
     },
     {
+        id: 6,
+        name: "Taiba G.",
+        specialty: "Functional Medicine Practitioner",
+        imageUrl: "/images/practioners/taiba-g.jpg",
+        profileUrl: "/practitioners/taiba-gul-functional-medicine-practitioner-united-kingdom/r/9tEiqAcaUvhaPr",
+        bio: "Having trained in Conventional Medicine, I very quickly realised that the approach to people needs to be from a holistic point of view. I moved in to General Practice and started looking at all the factors that were impacting my patients including their lifestyle, their diet and their wider environment. My focus on health promotion and my background in health coaching worked very well for my patients and I found my passion. This lead me to training under the Institute of Functional Medicine where I learnt to look for the ‘root cause’ behind the ‘pathology’.",
+        ratings: "5",
+    },
+    {
         id: 7,
-        name: "Steven F.",
-        specialty: "Mind-Body Wellness Practitioner",
-        imageUrl: "/images/practioners/steven-f.jpg",
+        name: "Christelle S.",
+        specialty: "Functional Medicine Practitioner",
+        imageUrl: "/images/practioners/christelle-s.jpg",
         profileUrl:
-            "https://book.healguid.com/practitioners/steven-ford-mind-body-wellness-practitioner-united-kingdom/r/p9LPx6kTOHj95W",
-        bio: "Fourteen years experience in a wide range of therapeutic settings has taught me that change lasts when we consider the whole person—mind, body and relationships. I've worked closely with individuals, groups, families and carers, and have seen how progress accelerates when we explore lived experience and, if applicable, any unresolved trauma, nervous system regulation and related challenging emotions such as shame, guilt, anger, grief, anxiety, depression and/or self esteem. My approach is trauma-informed and practical. I draw on ACT, DBT-informed skills, mindfulness and somatic awareness, together with Internal Family Systems and strengths-based coaching. We’ll clarify your goals and values, explore the systems you’re part of, and use tools you can apply between sessions. Clients value the psychological safety, steady pace and focus on real-life change. My aim is to empower you to lead your own recovery while I guide, challenge and support along the way.",
+            "/practitioners/christelle-stockdale-functional-medicine-practitioner-united-kingdom/r/en5dEK0cxgsVgw",
+        bio: "I was drawn to holistic and functional medicine because I wanted to help people get to the true root of their health issues. Not just manage symptoms but understand why they’re unwell and how to restore balance naturally. What makes my approach unique? I combine deep listening with detailed investigation. My focus is on treating the whole person, creating clear, realistic plans, and empowering my patients to take charge of their own health. What do patients value most? They appreciate that I truly listen, explain things clearly, and work with them to make treatment plans that are manageable. Many say they finally feel seen, understood, and supported. ",
         ratings: "5",
     },
     {
@@ -74,18 +68,15 @@ export const practitioners = [
         name: "Ayiesha M.",
         specialty: "Integrative Medicine Practitioner",
         imageUrl: "/images/practioners/ayeisha-m.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/ayiesha-malik-integrative-medicine-practitioner-united-kingdom/r/fbRpePhVpgaOdg",
+        profileUrl: "/practitioners/ayiesha-malik-integrative-medicine-practitioner-united-kingdom/r/fbRpePhVpgaOdg",
         bio: "As a dedicated GP with a passion for empowering patients to take control of their health, I've always sought to provide more than just conventional care. My journey into Functional Medicine (FM) began with a desire to address the root causes of illness, rather than just treating symptoms. I realized that by considering the intricate web of factors influencing a patient's well-being - from hormonal imbalances and gut health to lifestyle choices and environmental toxins - I could offer a more comprehensive and compassionate approach to healthcare. My patients appreciate the time I take to listen to their unique stories, and the personalized care plans we co-create to optimize their function and promote lasting wellness. By integrating the latest scientific research with time-honored principles of natural healing, I've witnessed firsthand the transformative power of FM in helping individuals reclaim their vitality and live more vibrant lives. Whether you're seeking relief from chronic illness or simply striving to enhance your overall well-being, I invite you to experience the difference that Functional Medicine can make in your life.",
-        ratings: "5",
     },
     {
         id: 9,
         name: "Lara S.",
         specialty: "Nutritional Therapist",
         imageUrl: "/images/practioners/lara-s.jpg",
-        profileUrl:
-            "https://book.healguid.com/practitioners/lara-seago-nutritional-therapist-united-kingdom/r/7ZY66s3ir9lkPi",
+        profileUrl: "/practitioners/lara-seago-nutritional-therapist-united-kingdom/r/7ZY66s3ir9lkPi",
         bio: " A number of things led me into Nutritional therapy / functional medicine. I had always had an interest in nutrition, largely stemming from my mum dying very young from stomach cancer and wanting to prevent the same happening to me. Then along the way different life experiences and some of my own health challenges, particularly in relation to stress, burnout, and hormonal imbalances motivated me to support people with the same challenges. Over the years I have worked with all kinds of clients and conditions and I am also cancer trained and able to support cancer patients alongside their mainstream treatment. Women's Health, Eating Disorders, Sport Nutrition, Cancer (breast cancer)",
         ratings: "5",
     },
